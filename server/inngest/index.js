e4d13719ade585508146e7230ce2b77176fa623e
Aditi <aditianand748@gmail.com>
2026-05-05 08:57:17 +0530

    fix cron + inngest bugs

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -96,7 +96,7 @@ const leaveApplicationReminder = inngest.createFunction(
 );
 // Cron : Check attendance at 11:30 AM IST (06: 00 UTC) and email absent employees
 const attendanceReminderCron = inngest.createFunction(
-  { id: "attendance-reminder-cron", triggers: [{cron: "0 0 6 * * *"}] },
+  { id: "attendance-reminder-cron", triggers: [{cron: "0 6 * * *"}] },
    // 06:00 UTC = 11:30 AM IST
   async ({ step}) => {
      // Step 1: GEt todays date range (IST)
